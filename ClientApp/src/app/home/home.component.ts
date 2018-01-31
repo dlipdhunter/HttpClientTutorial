9c452013336c0193b57794fe738d75fce055bc5e
Dillikumar Selvamani <dillikumar.s@congruentindia.com>
2018-01-31 19:01:42 +0530

    Add HttpClient Get method samples

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Pirate } from "./pirate.model";
+import { HomeService } from "./home.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  providers: [HomeService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit 
+{  
+  constructor(private homeService: HomeService) 
+  { 
+  }
+
+  ngOnInit(): void 
+  {
+    // this.homeService.GetAll();
+    // this.homeService.GetAll2();
+    // this.homeService.GetAll3();
+
+    // this.homeService.GetById(1);
+    // this.homeService.GetByIdWithParams(1);
+    // this.homeService.GetByIdWithHeaders(1);
+
+    let pirate: Pirate = {
+      id: 0,
+      name : "Roronova Zoro",
+      nickName : "Pirate hunter",
+      crewName : "Straw Hat Pirates",
+      position : "Swordsman",
+      bounty: 320000000
+    }
+
+    this.homeService.Create(pirate);
+
+  }
+
 }
--- ClientApp/src/app/home/home.service.ts
+++ ClientApp/src/app/home/home.service.ts
@@ -1,48 +1,127 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pirate } from './pirate.model';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class HomeService {
-    // Todo: update the api url
-    strawHatsApiUrl = '';
+    private strawHatsApiUrl = 'http://localhost:5000/api/strawhats/';
 
     constructor(private http: HttpClient) {
     }
 
+    /*
+    *   All HttpClient methods returns Observable of something. 
+    *   We could tell HttpClient to return Observable of our own types
+    */
+
+
+    // By default, httpClient returns the response body.
     GetAll() {
+        this.http.get(this.strawHatsApiUrl)
+            .subscribe(
+            (res) => {
+                console.log(res);
+            },
+            err => {
+                console.error(err);
+            });
+    }
+
+    GetAll2() {
         this.http.get<Pirate[]>(this.strawHatsApiUrl)
-        .subscribe(
+            .subscribe(
             (res: Pirate[]) => {
                 console.log(res);
             },
             err => {
-                console.log(err);
+                console.error(err);
             },
             () => {
                 console.log('Completed');
-            }
-        );
+            });
     }
 
-    /* By default, httpClient returns the body.
+    /* 
      * If we need to check the status code or headers then
-     * we need to pass an object with observe property set to response to tell httpClient to return the response.
+     * we need to pass an object with observe property set to 'response' to tell httpClient to return the response.
     */
-    GetAll2() {
+    GetAll3() {        
         this.http.get<Pirate[]>(this.strawHatsApiUrl, { observe: 'response' })
-        .subscribe(
+            .subscribe(
             (res) => {
+                console.log(res);
                 console.log(res.status);
-                console.log(res.headers);
                 console.log(res.body);
             },
             err => {
-                console.log(err);
-            }
-        );
+                console.error(err);
+            });
+    }
+
+    GetById(id: number) {
+
+        this.http.get<Pirate>(this.strawHatsApiUrl + id)
+        .subscribe(
+            res => {
+                console.log(res);
+            },
+            err => {
+                console.error(err);
+            });
+    }
+
+    /*
+    * In angular, HttpParams and HttpHeaders are immutable.
+    */
+
+    GetByIdWithParams(id: number) {               
+        // This doesn't work.
+        // let httpParams = new HttpParams();
+        // httpParams.set("name", "somename");
+        // httpParams.set("crewname", "somecrewname");
+        
+        // This does.
+        // let httpParams = new HttpParams();
+        // httpParams = httpParams.set("name", "somename");
+        // httpParams = httpParams.set("crewname", "somecrewname");
+
+        // or use chaining
+        let httpParams = new HttpParams().set("name", "somename").set("crewname", "somecrewname");
+
+        this.http.get<Pirate>(this.strawHatsApiUrl + id, { params: httpParams })
+        .subscribe(
+            res => {
+                console.log(res);
+            },
+            err => {
+                console.error(err);
+            });
+    }
+
+    GetByIdWithHeaders(id: number) {
+
+        let httpHeaders = new HttpHeaders().set("authorization", "myAuthKey");
+
+        this.http.get<Pirate>(this.strawHatsApiUrl + id, { headers: httpHeaders })
+        .subscribe(
+            res => {
+                console.log(res);
+            },
+            err => {
+                console.error(err);
+            });
+    }
+
+    Create(pirate: Pirate){
+        this.http.post(this.strawHatsApiUrl, pirate)
+        .subscribe(
+            res => {
+                console.log(res);
+            },
+            err => {
+                console.error(err);
+            });
     }
 
 }
-
